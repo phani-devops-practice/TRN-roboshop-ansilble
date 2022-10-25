fbdaa2748b9726402bf3e28e356c9224e246a3ad
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-25 07:17:55 +0530

    updating ./TRN-roboshop-ansilble

--- launch-instance-script/create-ec2.sh
+++ launch-instance-script/create-ec2.sh
@@ -15,7 +15,7 @@ COMPONENT=all
 create_ec2() {
   PRIVATE_IP=$(aws ec2 run-instances \
       --image-id ${AMI_ID} \
-      --instance-type t2.micro \
+      --instance-type t3.micro \
       --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}${ENV}}, {Key=Monitor,Value=yes}]" \
       --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}"\
       --security-group-ids ${SGID} \
